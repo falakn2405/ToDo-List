dbfaa630f859f5dbb2164288e23114ea99b8f41b
falakn2405 <falakn2405@gmail.com>
2023-10-11 11:46:21 +0500

    Removed temporary data

--- ToDo-List/App.js
+++ ToDo-List/App.js
@@ -1,79 +1,97 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, FlatList, Modal } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, FlatList, Modal, ActivityIndicator } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import colors from './Colors';
-import tempData from './tempData';
 import TodoList from './components/ToDoList';
 import AddListModal from './components/AddListModal';
 import Fire from './Fire';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   state ={ 
     addTodoVisible: false,
-    lists: tempData,
-    user: {}
+    lists: [],
+    loading: true,
   }
   componentDidMount() {
-    this.firebase = new Fire((error, user) => {
-      if(error) { return alert('Uh oh, something went wrong!')}
-
-      this.setState({ user });
+    firebase = new Fire((error, user) => {
+      if (error) {
+        console.error("Firebase initialization error:", error);
+        return alert('Uh oh, something went wrong during Firebase initialization!');
+      }
+  
+      firebase.getLists(lists => {
+        if (lists === null) {
+          console.error("Error fetching lists from Firebase or lists are empty.");
+        } else {
+          this.setState({ lists, loading: false });
+        }
+      });
     });
+  }
+  
+  componentWillUnmount() {
+    firebase.detach();
   }
   toggleAddTodoModal() {
     this.setState({ addTodoVisible: !this.state.addTodoVisible });
   }
-  renderList = list => {
+  renderList = list => { 
     return <TodoList list={list} updateList={this.updateList} />
-  }
-  addList = list => {
-    this.setState({ lists: [...this.state.lists, {...list, id: this.state.lists.length + 1, todos: [] }]})
+  } 
+  addList = list => { 
+    firebase.addList({
+      name: list.name,
+      color: list.color,
+      todos: []
+    })
   }
   updateList = list => {
-    this.setState({
-      lists: this.state.lists.map(item => {
-        return item.id === list.id ? list : item;
-      })
-    })
+    firebase.updateList(list);
   }
 
   render() {
+    if(this.state.loading) {
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator size='large' color={colors.black} />
+        </View>
+      )
+    }
     return (
-      <View style={styles.container}>
-        <Modal animationType='slide' visible={this.state.addTodoVisible} onRequestClose={() => this.toggleAddTodoModal()}>
-          <AddListModal closeModal={() => this.toggleAddTodoModal()} addList={this.addList} />
-        </Modal>
-
-        <View>
-          <Text>User: {this.state.user.uid}</Text>
+      <GestureHandlerRootView style={{flex:1}}>
+        <View style={styles.container}>
+          <Modal animationType='slide' visible={this.state.addTodoVisible} onRequestClose={() => this.toggleAddTodoModal()}>
+            <AddListModal closeModal={() => this.toggleAddTodoModal()} addList={this.addList} />
+          </Modal>
+          
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.divider} />
+            <Text style={styles.title}>
+              Todo <Text style={{ fontWeight: '300', color: colors.blue}}>Lists</Text>
+            </Text>
+            <View style={styles.divider} />
+          </View>
+    
+          <View style={{ marginVertical: 48 }}>
+            <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
+              <AntDesign name='plus' size={16} color={colors.blue} />
+            </TouchableOpacity>
+            <Text style={styles.add}>Add List</Text>
+          </View>
+    
+          <View style={{ height: 275, paddingLeft: 32}}>
+            <FlatList 
+              data={this.state.lists}
+              keyExtractor={item => item.id.toString()}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              renderItem={({ item }) => this.renderList(item) }
+              keyboardShouldPersistTaps='always'
+            />
+          </View>
         </View>
-
-        <View style={{ flexDirection: 'row' }}>
-          <View style={styles.divider} />
-          <Text style={styles.title}>
-            Todo <Text style={{ fontWeight: '300', color: colors.blue}}>Lists</Text>
-          </Text>
-          <View style={styles.divider} />
-        </View>
-  
-        <View style={{ marginVertical: 48 }}>
-          <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
-            <AntDesign name='plus' size={16} color={colors.blue} />
-          </TouchableOpacity>
-          <Text style={styles.add}>Add List</Text>
-        </View>
-  
-        <View style={{ height: 275, paddingLeft: 32}}>
-          <FlatList 
-            data={this.state.lists}
-            keyExtractor={item => item.name}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            renderItem={({ item }) => this.renderList(item) }
-            keyboardShouldPersistTaps='always'
-          />
-        </View>
-      </View>
+      </GestureHandlerRootView>
     );
   }
 }
